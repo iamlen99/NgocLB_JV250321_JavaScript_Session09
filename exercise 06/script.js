b3986142c6d1747f4af72a605277cd976f8beded
iamlen99 <allblue2903@gmail.com>
2025-05-21 12:07:37 +0700

    Update bài tập session 09 - 6

--- exercise 06/script.js
+++ exercise 06/script.js
@@ -11,40 +11,12 @@ let todoList = document.getElementById("myUL");
 function showTodoList() {
   let showTodos = "";
   for (let i = 0; i < myTodoList.length; i++) {
-    let todo = `<li class="addChecked-el${i}" onclick = "addChecked(${i})">${myTodoList[i]} 
-                <span class="close" onClick="deleteTodos(${i}, event)">X</span>
+    let todo = `<li>${myTodoList[i]} 
+                <span class="close">X</span>
                 </li>`;
     showTodos += todo;
   }
   todoList.innerHTML = showTodos;
 }
 
-function getInputValues() {
-  let inputTodo = document.getElementById("myInput");
-  let inputValue = inputTodo.value.trim();
-  if (inputValue === "") {
-    return;
-  }
-  myTodoList.push(inputValue);
-  inputTodo.value = "";
-  showTodoList();
-}
-
-function addTodos() {
-  let addTodo = document.querySelector(".addBtn");
-  addTodo.addEventListener("click", getInputValues);
-}
-
-function deleteTodos(index, event) {
-  event.stopPropagation();
-  myTodoList.splice(index, 1);
-  showTodoList();
-}
-
-function addChecked(index) {
-  let addCheckedElement = document.querySelector(`.addChecked-el${index}`);
-  addCheckedElement.classList.toggle("checked");
-}
-
 showTodoList();
-addTodos();
