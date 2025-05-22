39dfbbd811350de3ee114aa9492a5f25d4fcb835
iamlen99 <allblue2903@gmail.com>
2025-05-22 19:55:40 +0700

    Upload bài tập session 09 - 4

---
+++ exercise 04/script.js
@@ -0,0 +1,6 @@
+let div = document.getElementById("div");
+
+let convertButton = document.getElementById("convert-button");
+convertButton.onclick = function () {
+  div.classList.toggle("dark-mode");
+};
